fef3e6d3fad12bca7c80df8e942ba30faa1c2361
Previtali_Fabio <fabprev93@gmail.com>
2019-12-17 11:34:38 +0100

    Fatto voto in stelline!

--- main.js
+++ main.js
@@ -30,11 +30,24 @@ $(document).ready(function(){
                         var template_function = Handlebars.compile(template_html);
                         // Definiamo le variabili che verranno utilizzate nel template handlebars:
                         var current_selection = data_success.results[i];
+                        // GESTIONE DEL VOTO IN STELLINE: INIZIO
+                        var voto_stelline = Math.ceil(current_selection.vote_average/2);
+                        var stelline = "";
+                        console.log(voto_stelline);
+                        for (var j = 1; j <= 5; j++) {
+                            if (j <= voto_stelline) {
+                                stelline += '<i class="fas fa-star"></i>'
+                            } else {
+                                stelline += '<i class="far fa-star"></i>'
+                            };
+                        };
+                        // GESTIONE DEL VOTO IN STELLINE: FINE
                         var variabili = {
                             "title":current_selection.title,
                             "original_title":current_selection.original_title,
                             "original_language":current_selection.original_language,
-                            "vote_average":current_selection.vote_average
+                            "vote_average":current_selection.vote_average,
+                            "stelline":stelline
                         };
                         // Ingetto le variabili nella funzione template:
                         var html_finale = template_function(variabili);
