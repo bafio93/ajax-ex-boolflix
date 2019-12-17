36967d460da35182c0e13d8e49ef44ff05e6fe65
Previtali_Fabio <fabprev93@gmail.com>
2019-12-17 17:59:35 +0100

    MILESTONE 2 OK, PROBLEMI CON BANDIERE E STRUTTURARE MEGLIO CODICE!!! :)

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         var testo_input = $("input").val();
         // Con questo if, prendiamo il valore solo se non è vuoto!
         if (testo_input.length!=0) {
-// PARTE DELLA CHIAMATA API: INIZIO
+// PARTE DELLA CHIAMATA API FILM: INIZIO
             var my_url = "https://api.themoviedb.org/3";
             $.ajax({
                 "url": my_url+"/search/movie/",
@@ -42,11 +42,20 @@ $(document).ready(function(){
                             };
                         };
                         // GESTIONE DEL VOTO IN STELLINE: FINE
+                        // GESTIONE DELLA BANDIERINA: INIZIO
+                        // UTILIZZIAMO CODICE PRESENTE NEL SITO: https://www.countryflags.io/#body
+                        // PROBLEMA: EN PER ENGLISH, MA ESISTE BANDIERA! -> DIVENTA gb.
+                        var codice_bandiera = "https://www.countryflags.io/";
+                        if (current_selection.original_language=="en") {
+                            codice_bandiera += "gb/flat/64.png";
+                        } else {
+                            codice_bandiera += current_selection.original_language + "/flat/32.png";
+                        }
+                        // GESTIONE DELLA BANDIERINA: FINE
                         var variabili = {
                             "title":current_selection.title,
                             "original_title":current_selection.original_title,
-                            "original_language":current_selection.original_language,
-                            "vote_average":current_selection.vote_average,
+                            "original_language":codice_bandiera,
                             "stelline":stelline
                         };
                         // Ingetto le variabili nella funzione template:
@@ -58,7 +67,69 @@ $(document).ready(function(){
                     alert("ERROR! -.-");
                 }
             });
-// PARTE DELLA CHIAMATA API: FINE
+// PARTE DELLA CHIAMATA API FILM: FINE
+// PARTE DELLA CHIAMATA API SERIE TV: INIZIO
+            $.ajax({
+                "url": my_url+"/search/tv/",
+                "data":{
+                    // Questa è la mia chiave!
+                    "api_key":"b05b883f04e9f1073304ba663a32e331",
+                    // Qui inserirò testo_input:
+                    "query":testo_input
+                },
+                "method":"GET",
+                "success": function(data_success){
+// FUNZIONE DI SUCCESS NELLA AJAX!
+                    // supponiamo di volere stampare in console il titolo dei film trovati:
+                    console.log(data_success);
+                    // Questo ciclo FOR è necessario per creare le mie schede film!
+                    for (var i = 0; i < data_success.results.length; i++) {
+// TEMPLATE HANDLEBARS
+                        // Inizializzo il template handlebars
+                        var template_html = $("#template-film").html();
+                        var template_function = Handlebars.compile(template_html);
+                        // Definiamo le variabili che verranno utilizzate nel template handlebars:
+                        var current_selection = data_success.results[i];
+                        // GESTIONE DEL VOTO IN STELLINE: INIZIO
+                        var voto_stelline = Math.ceil(current_selection.vote_count/2);
+                        var stelline = "";
+                        console.log(voto_stelline);
+                        for (var j = 1; j <= 5; j++) {
+                            if (j <= voto_stelline) {
+                                stelline += '<i class="fas fa-star"></i>'
+                            } else {
+                                stelline += '<i class="far fa-star"></i>'
+                            };
+                        };
+                        // GESTIONE DEL VOTO IN STELLINE: FINE
+// GESTIONE DELLA BANDIERINA: INIZIO
+                        // UTILIZZIAMO CODICE PRESENTE NEL SITO: https://www.countryflags.io/#body
+                        // PROBLEMA: EN PER ENGLISH, MA ESISTE BANDIERA! -> DIVENTA gb.
+                        var codice_bandiera = "https://www.countryflags.io/";
+                        if (current_selection.original_language=="en") {
+                            codice_bandiera += "gb/flat/64.png";
+                        } else if (current_selection.original_language.length==0) {
+                            codice_bandiera = "";
+                        } else {
+                            codice_bandiera += current_selection.original_language + "/flat/32.png";
+                        }
+// GESTIONE DELLA BANDIERINA: FINE
+                        var variabili = {
+                            "title":current_selection.name,
+                            "original_title":current_selection.original_name,
+                            "original_language":codice_bandiera,
+                            "stelline":stelline
+                        };
+                        // Ingetto le variabili nella funzione template:
+                        var html_finale = template_function(variabili);
+                        $(".film-container").append(html_finale);
+                    };
+                },
+                "error": function(){
+                    alert("ERROR! -.-");
+                }
+            });
+// PARTE DELLA CHIAMATA API SERIE TV: FINE
             // RESET FINALE DEL VALORE NEL CAMPO INPUT
             $("input").val("");
         }
